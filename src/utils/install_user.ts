912189874cca415eac06de11fb82f49d3e395144
ElTurbinas <73681786+ElTurbinas@users.noreply.github.com>
2022-06-24 02:33:16 +0000

    Archivo sin funcion eliminado

--- src/utils/install_user.ts
+++
@@ -1,8 +0,0 @@
-import { Guild, antiRF } from '../database/BotDataBase'
-import { version } from '../../package.json'
-import discord from 'discord.js'
-import { ExtendedClient } from '../structures/Client';
-
-export async function install_user(client: ExtendedClient, user: discord.User) {
-    
-}
